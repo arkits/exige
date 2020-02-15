9c6894195a80c3e680d61884df0dd0fc777d72f8
Archit Khode <arkits@outlook.com>
2020-02-14 17:52:50 -0800

    Updates

---
+++ precache-manifest.b30cba39e5867794fb3ead64380b68e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a4627c65e0e8248547e5ed699884ae",
+    "url": "/exige-react/index.html"
+  },
+  {
+    "revision": "b96a360ac17d06ee1156",
+    "url": "/exige-react/static/css/main.11aacdfc.chunk.css"
+  },
+  {
+    "revision": "22e861837ecf486dfda8",
+    "url": "/exige-react/static/js/2.c749a8a7.chunk.js"
+  },
+  {
+    "revision": "864fa493538e36ec10ed73cebe68a079",
+    "url": "/exige-react/static/js/2.c749a8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96a360ac17d06ee1156",
+    "url": "/exige-react/static/js/main.16b0facf.chunk.js"
+  },
+  {
+    "revision": "e0f7747176453e3c9a2a",
+    "url": "/exige-react/static/js/runtime-main.22b2379e.js"
+  }
+]);
