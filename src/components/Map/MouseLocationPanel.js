7f22ef9109d7232dffb08dfc69017d1e054ceee2
Archit Khode <arkits@outlook.com>
2020-02-15 16:53:25 -0800

    refactor MouseLocationPanel - use Frame instead of Project

--- src/components/Map/MouseLocationPanel.js
+++ src/components/Map/MouseLocationPanel.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { Project } from 'arwes';
+import { Frame } from 'arwes';
 
 function MouseLocationPanel({ mouseLocation, zoomLevel }) {
+    const calcMouseLocationDetail = () => {
 
-    let latitude_rad = Math.PI * (mouseLocation['latitude'] / 180);
+        if(mouseLocation['latitude'] == null || mouseLocation['longitude'] == null){
+            return {};
+        }
 
-    let n = 2.0 ** zoomLevel;
+        // Conversion from co-ordinate to Slippy X/Y adapted from
+        // https://github.com/interuss/dss/blob/legacy/tcl4/datanode/src/slippy_util.py  
+        let latitude_rad = Math.PI * (mouseLocation['latitude'] / 180);
+        let n = 2.0 ** zoomLevel;
+        let xtile = Math.trunc(((mouseLocation['longitude'] + 180.0) / 360.0) * n);
+        let ytile = Math.trunc(((1.0 - Math.log(Math.tan(latitude_rad) + 1 / Math.cos(latitude_rad)) / Math.PI) / 2.0) * n);
 
-    let xtile = Math.trunc(((mouseLocation['longitude'] + 180.0) / 360.0 * n));
-    
-    let ytile = Math.trunc(
-        ((1.0 - Math.log(Math.tan(latitude_rad) + 1 / Math.cos(latitude_rad)) / Math.PI) / 2.0) * n
-    );
+        mouseLocation['x'] = xtile;
+        mouseLocation['y'] = ytile;
+        mouseLocation['zoomLevel'] = parseInt(zoomLevel);
 
-    mouseLocation['x'] = xtile;
-    mouseLocation['y'] = ytile;
-    mouseLocation['zoomLevel'] = zoomLevel;
+        return mouseLocation;
+    };
 
     return (
-        <div className="MapPanel">
-
-            <Project header="Mouse Location" icon={null}>
-                <pre style={{ fontSize: 12 }}>{JSON.stringify(mouseLocation, null, 2)}</pre>
-            </Project>
+        <div className="MouseLocationPanel">
+            <Frame corners={4} layer="primary">
+                <div style={{ padding:'10px 20px' }}>
+                    <pre style={{ fontSize: 14 }}>
+                        {JSON.stringify(calcMouseLocationDetail(), null, 2)}
+                    </pre>
+                </div>
+            </Frame>
         </div>
     );
 }
