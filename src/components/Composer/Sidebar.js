013513ead5687182fa8c67062161b57f3cc49a9e
Archit Khode <arkits@outlook.com>
2020-02-16 20:32:05 -0800

    implemented commiting to Store from Composer Sidebar

--- src/components/Composer/Sidebar.js
+++ src/components/Composer/Sidebar.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Project } from 'arwes';
+import { useObserver } from 'mobx-react';
+import { StoreContext } from '../../store/ExigeStore';
 
 function Sidebar({ data }) {
-    const displayOperationVolumes = features => {
+    const OperationsVolumes = ({ features }) => {
         let operationVolumes = [];
 
-        if (features != null) {
+        const store = React.useContext(StoreContext);
+
+        if (features !== null && features.length > 0) {
             features.forEach(feature => {
                 operationVolumes.push(feature.geometry);
             });
+
+            store.operations[0] = features;
         }
 
-        return JSON.stringify(operationVolumes, null, 2);
+        return (
+            <textarea
+                className="composerSidebarTextarea"
+                value={JSON.stringify(operationVolumes, null, 2)}
+            />
+        );
     };
 
     return (
         <div className="fh">
             <Project className="fh" animate header="Composer" icon={null}>
-                <textarea className="composerSidebarTextarea" value={displayOperationVolumes(data)} />
+                <OperationsVolumes features={data} />
             </Project>
         </div>
     );
