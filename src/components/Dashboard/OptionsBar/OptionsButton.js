e12a30e3c876a6080e61c4b5999e5a5dd9e919ee
Archit Khode <arkits@outlook.com>
2020-03-01 14:54:01 -0800

    removed unused import

--- src/components/Dashboard/OptionsBar/OptionsButton.js
+++ src/components/Dashboard/OptionsBar/OptionsButton.js
@@ -7,7 +7,6 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import Icon from '@material-ui/core/Icon';
-import Typography from '@material-ui/core/Typography';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
