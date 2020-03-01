e8cb654d35ca1084eb29380f78bc00394bf52687
Archit Khode <arkits@outlook.com>
2020-03-01 14:50:40 -0800

    added elementtoggle to optionsbar + refactor

--- src/components/Dashboard/OptionsBar/OptionsBar.js
+++ src/components/Dashboard/OptionsBar/OptionsBar.js
@@ -3,14 +3,16 @@ import Button from '@material-ui/core/Button';
 import { observer } from 'mobx-react';
 import { AskariStoreContext } from '../../../store/AskariStore';
 import OptionsButton from './OptionsButton';
+import ElementToggle from './ElementToggle';
 
 const OptionsBar = observer(() => {
     const askariStore = useContext(AskariStoreContext);
 
     return (
         <div className="OptionsBar">
-
             <OptionsButton />
+
+            <ElementToggle />
 
             <div
                 className="leftStatus"
@@ -20,21 +22,15 @@ const OptionsBar = observer(() => {
                 }}
             >
                 <Button
-                    variant="text"
-                    style={{ textTransform: 'none', fontFamily: 'IBM Plex Mono' }}
-                >
-                    SocketIO: {askariStore.sioStatus}
-                </Button>
-                <Button
-                    variant="text"
+                    variant="contained"
                     style={{
-                        fontFamily: 'IBM Plex Mono',
-                        fontWeight: 'bold',
                         textTransform: 'none',
-                        fontStyle: 'italic'
+                        fontFamily: 'IBM Plex Mono',
+                        backgroundColor: askariStore.sioStatus === 'CONNECTED' ? 'green' : 'red',
+                        fontWeight: 'bold'
                     }}
                 >
-                    ~/Exige Reborn
+                    S.IO: {askariStore.sioStatus}
                 </Button>
             </div>
         </div>
