3d38692bbb5c4b5c9b16e81053284134460aa364
Archit Khode <arkits@outlook.com>
2020-03-01 14:51:38 -0800

    made elements reactive to elementtoggle

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,42 @@
-import React from "react";
-import { observer } from "mobx-react";
+import React, { useContext } from 'react';
+import { observer } from 'mobx-react';
+import { AskariStoreContext } from '../../store/AskariStore';
+import MouseLocationPanel from './MouseLocationPanel';
+import OptionsBar from './OptionsBar/OptionsBar';
+import Map from './Map';
+import PositionsTable from './PositionsTable';
 
-import MouseLocationPanel from "./MouseLocationPanel";
-import OptionsBar from "./OptionsBar/OptionsBar";
-import Map from "./Map";
-import PositionsTable from "./PositionsTable";
+import './Dashboard.css';
 
-import "./Dashboard.css";
+const RenderMouseLocationPanel = observer(() => {
+    const askariStore = useContext(AskariStoreContext);
 
-const Dashboard = observer(() => {
-  return (
-    <div className="Dashboard">
-      <Map />
-      <MouseLocationPanel />
-      <PositionsTable />
-      <OptionsBar />
-    </div>
-  );
+    if (askariStore.elementsToggle.mouseLocation) {
+        return <MouseLocationPanel />;
+    } else {
+        return null;
+    }
 });
+
+const RenderPositionsTable = observer(() => {
+    const askariStore = useContext(AskariStoreContext);
+
+    if (askariStore.elementsToggle.positionsTable) {
+        return <PositionsTable />;
+    } else {
+        return null;
+    }
+});
+
+const Dashboard = () => {
+    return (
+        <div className="Dashboard">
+            <Map />
+            <RenderMouseLocationPanel />
+            <RenderPositionsTable />
+            <OptionsBar />
+        </div>
+    );
+};
 
 export default Dashboard;
--- src/components/Dashboard/MouseLocationPanel.js
+++ src/components/Dashboard/MouseLocationPanel.js
@@ -1,38 +1,42 @@
-import React, { useContext } from "react";
-import { observer } from "mobx-react";
-import { AskariStoreContext } from "../../store/AskariStore";
+import React, { useContext } from 'react';
+import { observer } from 'mobx-react';
+import { AskariStoreContext } from '../../store/AskariStore';
 
 const MouseLocationPanel = observer(() => {
+    const askariStore = useContext(AskariStoreContext);
 
-  const askariStore = useContext(AskariStoreContext);
+    const calcMouseLocationDetail = () => {
+        let mouseLocation = askariStore.mouseLocation;
+        let zoomLevel = askariStore.gridZoomLevel;
 
-  const calcMouseLocationDetail = () => {
+        // Conversion from co-ordinate to Slippy X/Y adapted from
+        // https://github.com/interuss/dss/blob/legacy/tcl4/datanode/src/slippy_util.py
 
-    let mouseLocation = askariStore.mouseLocation;
-    let zoomLevel = askariStore.gridZoomLevel;
+        let latitudeRad = Math.PI * (mouseLocation['lat'] / 180);
+        let n = 2.0 ** zoomLevel;
+        let xtile = Math.trunc(((mouseLocation['lng'] + 180.0) / 360.0) * n);
+        let ytile = Math.trunc(
+            ((1.0 - Math.log(Math.tan(latitudeRad) + 1 / Math.cos(latitudeRad)) / Math.PI) / 2.0) *
+                n
+        );
 
-    // Conversion from co-ordinate to Slippy X/Y adapted from
-    // https://github.com/interuss/dss/blob/legacy/tcl4/datanode/src/slippy_util.py 
+        mouseLocation['x'] = xtile;
+        mouseLocation['y'] = ytile;
+        mouseLocation['zoomLevel'] = parseInt(zoomLevel);
 
-    let latitudeRad = Math.PI * (mouseLocation['lat'] / 180);
-    let n = 2.0 ** zoomLevel;
-    let xtile = Math.trunc(((mouseLocation['lng'] + 180.0) / 360.0) * n);
-    let ytile = Math.trunc(((1.0 - Math.log(Math.tan(latitudeRad) + 1 / Math.cos(latitudeRad)) / Math.PI) / 2.0) * n);
+        return mouseLocation;
+    };
 
-    mouseLocation['x'] = xtile;
-    mouseLocation['y'] = ytile;
-    mouseLocation['zoomLevel'] = parseInt(zoomLevel);
-
-    return mouseLocation;
-  };
-
-  return (
-    <div className="MouseLocationPanel">
-      <pre style={{ fontSize: 14 }}>
-        {JSON.stringify(calcMouseLocationDetail(), null, 2)}
-      </pre>
-    </div>
-  );
+    return (
+        <div className="MouseLocationPanel">
+            <div style={{ fontFamily: 'IBM Plex Mono', fontWeight: 'bold', fontStyle: 'italic' }}>
+                Mouse Location
+            </div>
+            <pre style={{ fontFamily: 'IBM Plex Mono' }}>
+                {JSON.stringify(calcMouseLocationDetail(), null, 2)}
+            </pre>
+        </div>
+    );
 });
 
 export default MouseLocationPanel;
