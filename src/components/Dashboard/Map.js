ae5510f3cbb99e586307b317640b6fff2efe718c
Archit Khode <arkits@outlook.com>
2020-03-01 15:59:21 -0800

    implemented drawing of positions

--- src/components/Dashboard/Map.js
+++ src/components/Dashboard/Map.js
@@ -1,54 +1,63 @@
-import React, { useContext } from "react";
-import ReactMapboxGl from "react-mapbox-gl";
-import { observer } from "mobx-react";
-import { AskariStoreContext } from "../../store/AskariStore";
+import React, { useContext } from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import { observer } from 'mobx-react';
+import { AskariStoreContext } from '../../store/AskariStore';
 
 const MapGl = ReactMapboxGl({
-  accessToken:
-    "pk.eyJ1IjoiYXJraXRzIiwiYSI6ImNqc3Bud29jMjAzcWc0OXJ6Y3YzOHltaTcifQ.EMMG5GSbT0T-lD8RGJgnAA"
+    accessToken:
+        'pk.eyJ1IjoiYXJraXRzIiwiYSI6ImNqc3Bud29jMjAzcWc0OXJ6Y3YzOHltaTcifQ.EMMG5GSbT0T-lD8RGJgnAA'
 });
 
 const Map = observer(() => {
-  const askariStore = useContext(AskariStoreContext);
+    const askariStore = useContext(AskariStoreContext);
 
-  const updateMouseLocation = (_, e) => {
-    askariStore.mouseLocation["lat"] = e["lngLat"]["lat"];
-    askariStore.mouseLocation["lng"] = e["lngLat"]["lng"];
-  };
+    const positions = Object.values(askariStore.positions);
 
-  const copyLatLng = (_, e) => {
-    
-    let coordinates = {
-      lat: e["lngLat"]["lat"],
-      lng: e["lngLat"]["lng"]
-    }
+    const updateMouseLocation = (_, e) => {
+        askariStore.mouseLocation['lat'] = e['lngLat']['lat'];
+        askariStore.mouseLocation['lng'] = e['lngLat']['lng'];
+    };
 
-    let strCoordinates = JSON.stringify(coordinates);
+    const copyLatLng = (_, e) => {
+        let coordinates = {
+            lat: e['lngLat']['lat'],
+            lng: e['lngLat']['lng']
+        };
 
-    console.log("Copied to clipboard - ", strCoordinates);
+        let strCoordinates = JSON.stringify(coordinates);
 
-    navigator.clipboard.writeText(strCoordinates);
+        console.log('Copied to clipboard - ', strCoordinates);
 
-    askariStore.snackbar.message = "Copied to co-ordinates to clipboard.";
-    askariStore.snackbar.isOpen = true;
-    
-  };
+        navigator.clipboard.writeText(strCoordinates);
 
-  return (
-    <div className="Map">
-      <MapGl
-        style="mapbox://styles/mapbox/dark-v9"
-        zoom={[9]}
-        center={[-122.43438720703125, 37.77722770873696]}
-        containerStyle={{
-          height: "calc(100vh - 70px)",
-          width: "100vw"
-        }}
-        onMouseMove={updateMouseLocation}
-        onClick={copyLatLng}
-      ></MapGl>
-    </div>
-  );
+        askariStore.snackbar.message = 'Copied to co-ordinates to clipboard.';
+        askariStore.snackbar.isOpen = true;
+    };
+
+    return (
+        <div className="Map">
+            <MapGl
+                style="mapbox://styles/mapbox/dark-v9"
+                // zoom={[9]}
+                // center={[-122.43438720703125, 37.77722770873696]}
+                containerStyle={{
+                    height: 'calc(100vh - 70px)',
+                    width: '100vw'
+                }}
+                onMouseMove={updateMouseLocation}
+                onClick={copyLatLng}
+            >
+                >
+                {positions.map(position => {
+                    return (
+                        <Layer type="symbol" key={position.vehicleId} id={position.vehicleId} layout={{ 'icon-image': 'airport-15' }}>
+                            <Feature id={position.vehicleId} coordinates={[position.longitude, position.latitude]} />
+                        </Layer>
+                    );
+                })}
+            </MapGl>
+        </div>
+    );
 });
 
 export default Map;
