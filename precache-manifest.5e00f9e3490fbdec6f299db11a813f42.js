5bb105da0f863a4510e7ed8c8413998d83fec187
Archit Khode <arkits@outlook.com>
2020-03-01 16:15:35 -0800

    Updates

--- precache-manifest.5e00f9e3490fbdec6f299db11a813f42.js
+++ precache-manifest.5e00f9e3490fbdec6f299db11a813f42.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e64471821253b2e8b5d4e2a1875c2e40",
+    "revision": "5ec4a2dc07d8081769000afc831bd6c0",
     "url": "/exige-react/index.html"
   },
   {
-    "revision": "ac97f20e406c9214a5dd",
+    "revision": "15dc38ababc46e611d66",
     "url": "/exige-react/static/css/2.3b80cb7c.chunk.css"
   },
   {
-    "revision": "6e85cc8aee2bdb7ffd53",
+    "revision": "c07f0b908955cb3b784d",
     "url": "/exige-react/static/css/main.97137445.chunk.css"
   },
   {
-    "revision": "ac97f20e406c9214a5dd",
-    "url": "/exige-react/static/js/2.75d16534.chunk.js"
+    "revision": "15dc38ababc46e611d66",
+    "url": "/exige-react/static/js/2.a231bc58.chunk.js"
   },
   {
     "revision": "20fde3f1dcac5a0886a03a28a776c1e6",
-    "url": "/exige-react/static/js/2.75d16534.chunk.js.LICENSE.txt"
+    "url": "/exige-react/static/js/2.a231bc58.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e85cc8aee2bdb7ffd53",
-    "url": "/exige-react/static/js/main.928a8ff2.chunk.js"
+    "revision": "c07f0b908955cb3b784d",
+    "url": "/exige-react/static/js/main.ba3f3f5b.chunk.js"
   },
   {
     "revision": "e0f7747176453e3c9a2a",
