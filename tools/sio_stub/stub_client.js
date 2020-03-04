9545afacbe5186633fd7fa59b4656630712097d6
Archit Khode <arkits@outlook.com>
2020-03-04 00:31:36 -0800

    added experimental traffic generator

--- tools/sio_stub/stub_client.js
+++ tools/sio_stub/stub_client.js
@@ -1,29 +1,101 @@
 const socket = require('socket.io-client')('http://127.0.0.1:8786');
 const randomLocation = require('random-location');
 
-let payload = {};
-
-const P = {
+// Epicenter of the simulaiton
+const epicenter = {
     latitude: 37.7768006,
     longitude: -122.4187928
 };
 
-const R = 5000; // meters
+// Radius of the simulation in meters
+const radius = 5000;
+
+// Number of Flight to simulate
+const numberOfFlights = 30;
+
+let flightPlans = [];
+
+// Initialize the flightPayloads
+for (var i = 0; i < numberOfFlights; i++) {
+    let flightPlan = generateFlightPlan(i);
+    console.log(flightPlan);
+    flightPlans[i] = flightPlan;
+}
+
+function generateFlightPlan(i) {
+    const randomPoint = randomLocation.randomCirclePoint(epicenter, radius);
+
+    const xAndY = generateXandY();
+
+    return {
+        id: i,
+        latDeg: randomPoint.latitude,
+        lonDeg: randomPoint.longitude,
+        x: xAndY.x,
+        y: xAndY.y,
+        cyclesLapsed: 0
+    };
+}
+
+function flyFlightPlan(flightPlan) {
+    let newLatDeg = flightPlan.latDeg + flightPlan.x / 1000;
+    let newLonDeg = flightPlan.lonDeg + flightPlan.y / 1000;
+
+    flightPlan.latDeg = newLatDeg;
+    flightPlan.lonDeg = newLonDeg;
+
+    flightPlan.cyclesLapsed = flightPlan.cyclesLapsed + 1;
+
+    return flightPlan;
+}
+
+function generateXandY(min, max) {
+    let possibleDirections = [-1, 0, 1];
+
+    let x = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
+    let y = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
+
+    while (x === 0 && y === 0) {
+        // both x and y are 0... the aircraft won't move...
+
+        let whoToRetryRandom = Math.floor(Math.random() * 2);
+
+        if (whoToRetryRandom === 0) {
+            x = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
+        } else {
+            y = possibleDirections[Math.floor(Math.random() * possibleDirections.length)];
+        }
+    }
+
+    return {
+        x: x,
+        y: y
+    };
+}
+
+function generatePayload(flightPlan) {
+    return {
+        data: {
+            vid: 'ARKITS' + String(flightPlan.id),
+            latDeg: flightPlan.latDeg,
+            lonDeg: flightPlan.lonDeg
+        },
+        metadata: {
+            sourceTimestamp: new Date().toISOString()
+        }
+    };
+}
 
 setInterval(function() {
-    for (var i = 0; i < 30; i++) {
-        const randomPoint = randomLocation.randomCirclePoint(P, R);
+    for (var i = 0; i < numberOfFlights; i++) {
+        let flightPlan = flightPlans[i];
 
-        payload = {
-            data: {
-                vid: 'ARKITS' + String(i),
-                latDeg: randomPoint.latitude,
-                lonDeg: randomPoint.longitude
-            },
-            metadata: {
-                sourceTimestamp: new Date().toISOString()
-            }
-        };
+        let newFlightPlan = flyFlightPlan(flightPlan);
+
+        // update flightPlans
+        flightPlans[i] = newFlightPlan;
+
+        let payload = generatePayload(newFlightPlan);
 
         console.log('Generated - ', payload);
 
