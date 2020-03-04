62f93671bfcfbca3619c0f1dd0adbbc34261f161
Archit Khode <arkits@outlook.com>
2020-03-03 22:41:06 -0800

    updated stub_client to use random-location package

--- tools/sio_stub/stub_client.js
+++ tools/sio_stub/stub_client.js
@@ -1,26 +1,32 @@
-let socket = require('socket.io-client')('http://127.0.0.1:8786');
+const socket = require('socket.io-client')('http://127.0.0.1:8786');
+const randomLocation = require('random-location');
 
 let payload = {};
 
-setInterval(function () {
+const P = {
+    latitude: 37.7768006,
+    longitude: -122.4187928
+};
 
+const R = 5000; // meters
+
+setInterval(function() {
     for (var i = 0; i < 30; i++) {
+        const randomPoint = randomLocation.randomCirclePoint(P, R);
 
         payload = {
             data: {
-                vid: "ARKITS" + String(i),
-                latDeg: Math.round((Math.random()*360 - 180) * 1000)/1000,
-                lonDeg: Math.round((Math.random()*360 - 180) * 1000)/1000
+                vid: 'ARKITS' + String(i),
+                latDeg: randomPoint.latitude,
+                lonDeg: randomPoint.longitude
             },
             metadata: {
                 sourceTimestamp: new Date().toISOString()
             }
         };
 
-        console.log("Generated - ", payload);
+        console.log('Generated - ', payload);
 
         socket.emit('brodcast_position', payload);
-
     }
-
 }, 1000);
