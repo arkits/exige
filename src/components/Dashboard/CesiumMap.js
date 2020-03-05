e503e10591cd05eba37f0e5517615c572f5e9b25
Khode, Archit (ARC-AFH)[UNIVERSITIES SPACE RESEARCH ASSN] <archit.khode@nasa.gov>
2020-03-04 16:54:09 -0800

    Update CesiumMap.js

--- src/components/Dashboard/CesiumMap.js
+++ src/components/Dashboard/CesiumMap.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Cartesian3, Transforms } from 'cesium';
 import { Viewer, CameraFlyTo, Model } from 'resium';
 import glb from '../../assets/Cesium_Air.glb';
+import { observer } from 'mobx-react';
+import { AskariStoreContext } from '../../store/AskariStore';
 
-function CesiumMap() {
-    
-    const origin = Cartesian3.fromDegrees(-122.43438720703125, 37.77722770873696, 2000.0);
-    const cameraDest = Cartesian3.fromDegrees(-122.43438720703125, 37.77722770873696, 210000);
-    const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
+const CesiumMap = observer(() => {
+
+    const askariStore = useContext(AskariStoreContext);
+
+    const positions = Object.values(askariStore.positions);
+
+    const cameraDest = Cartesian3.fromDegrees(-122.43438720703125, 37.77722770873696, 20000);
+
+    const RenderPositionModels = ({ position }) => {
+
+        console.log(position);
+
+        const origin = Cartesian3.fromDegrees(position.data.latDeg, position.data.lonDeg, 2000.0);
+
+        const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
+
+        return (
+            <Model
+                url={glb}
+                modelMatrix={modelMatrix}
+                minimumPixelSize={128}
+                maximumScale={20000}
+            />);
+    }
 
     return (
         <Viewer
@@ -20,15 +41,30 @@ function CesiumMap() {
             automaticallyTrackDataSourceClocks={false}
             projectionPicker={false}
         >
-            <CameraFlyTo destination={cameraDest} duration={0} />
-            <Model
-                url={glb}
-                modelMatrix={modelMatrix}
-                minimumPixelSize={128}
-                maximumScale={20000}
-            />
+
+            {positions.map(position => {
+
+                const origin = Cartesian3.fromDegrees(position.data.lonDeg, position.data.latDeg, 2000.0);
+
+                const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
+
+                return (
+                    <Model
+                        key={position.data.vid}
+                        id={position.data.vid}
+                        url={glb}
+                        modelMatrix={modelMatrix}
+                        minimumPixelSize={128}
+                        maximumScale={20000}
+                    />
+
+
+                );
+            })}
+
+
         </Viewer>
     );
-}
+});
 
 export default CesiumMap;
