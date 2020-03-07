be35b9e5bedd117bd99454741b3b2376490086d8
Archit Khode <arkits@outlook.com>
2020-03-06 20:26:29 -0800

    added operations to askaristore

--- src/store/AskariStore.js
+++ src/store/AskariStore.js
@@ -1,36 +1,38 @@
-import { observable, decorate } from "mobx";
-import { createContext } from "react";
+import { observable, decorate } from 'mobx';
+import { createContext } from 'react';
 
 class AskariStore {
-  sio = {
-    status: "DISCONNECTED",
-    isEnabled: false
-  };
-  positions = {};
-  mouseLocation = {};
-  snackbar = {
-    message: null,
-    isOpen: false,
-    type: null
-  };
-  elementsToggle = {
-    mouseLocation: true,
-    positionsTable: false
-  };
-  gridTiles = {
-    enabled: false,
-    tilesData: null,
-    zoomLevel: 10
-  }
+    sio = {
+        status: 'DISCONNECTED',
+        isEnabled: false
+    };
+    operations = {};
+    positions = {};
+    mouseLocation = {};
+    snackbar = {
+        message: null,
+        isOpen: false,
+        type: null
+    };
+    elementsToggle = {
+        mouseLocation: true,
+        positionsTable: false
+    };
+    gridTiles = {
+        enabled: false,
+        tilesData: null,
+        zoomLevel: 10
+    };
 }
 
 decorate(AskariStore, {
-  sio: observable,
-  positions: observable,
-  mouseLocation: observable,
-  gridZoomLevel: observable,
-  snackbar: observable,
-  elementsToggle: observable
+    sio: observable,
+    operations: observable,
+    positions: observable,
+    mouseLocation: observable,
+    gridZoomLevel: observable,
+    snackbar: observable,
+    elementsToggle: observable
 });
 
 export const AskariStoreContext = createContext(new AskariStore());
