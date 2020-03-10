2c75737617326828b2600578291e5636506956d9
Archit Khode <arkits@outlook.com>
2020-03-09 23:30:17 -0700

    implement setting creds

--- src/views/Inspector/Nuss/NussInspector.js
+++ src/views/Inspector/Nuss/NussInspector.js
@@ -2,39 +2,93 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Typography } from '@material-ui/core';
 import NussOperationsTable from './NussOperationsTable';
+import SetOperatorCreds from './SetOperatorCreds';
+import { useLocalStorage } from '../../../utils/storage';
 
 const NussInspector = () => {
     const [loadedOperations, setLoadedOperations] = useState([]);
 
+    const [creds, setCreds] = useLocalStorage('creds', {
+        username: '',
+        password: '',
+        url: ''
+    });
+
     useEffect(() => {
         axios({
             method: 'GET',
-            url: 'http://localhost:8786/nuss/operator/operations',
+            url: creds.url + '/operations',
             headers: {
                 'Acces-Control-Allow-Origin': '*',
-                Authorization: 'Basic =='
+                Authorization: 'Basic ' + btoa(creds.username + ':' + creds.password)
             }
         }).then(res => {
-            console.log('loaded');
+            console.log('Loaded!');
             setLoadedOperations(res.data);
         });
     }, []);
 
-    return (
-        <div className="NussInspector" style={{ paddingTop: '30px', paddingLeft: '50px' }}>
-            <Typography
-                variant="h4"
-                style={{ fontFamily: 'IBM Plex Mono', fontStyle: 'italic', fontWeight: 'bold' }}
-            >
-                Operations
-            </Typography>
+    if (creds.username !== '') {
+        return (
+            <div className="NussInspector" style={{ paddingTop: '30px', paddingLeft: '50px' }}>
+                <div style={{ display: 'flex' }}>
+                    <div style={{ flexGrow: '1' }}>
+                        <Typography
+                            variant="h4"
+                            style={{
+                                fontFamily: 'IBM Plex Mono',
+                                fontStyle: 'italic',
+                                fontWeight: 'bold'
+                            }}
+                        >
+                            Operations
+                        </Typography>
+                    </div>
 
-            <br />
-            <br />
+                    <div>
+                        <SetOperatorCreds setCreds={setCreds} />
+                    </div>
+                </div>
 
-            <NussOperationsTable operations={loadedOperations} />
-        </div>
-    );
+                <br />
+                <br />
+
+                <NussOperationsTable operations={loadedOperations} />
+            </div>
+        );
+    } else {
+        return (
+            <div className="NussInspector" style={{ paddingTop: '30px', paddingLeft: '50px' }}>
+                <div style={{ display: 'flex' }}>
+                    <div style={{ flexGrow: '1' }}>
+                        <Typography
+                            variant="h4"
+                            style={{
+                                fontFamily: 'IBM Plex Mono',
+                                fontStyle: 'italic',
+                                fontWeight: 'bold'
+                            }}
+                        >
+                            Operations
+                        </Typography>
+                    </div>
+
+                    <div>
+                        <SetOperatorCreds setCreds={setCreds} />
+                    </div>
+                </div>{' '}
+                <br />
+                <br />
+                <br />
+                <br />
+                <center>
+                    <Typography variant="h2" style={{ fontFamily: 'IBM Plex Mono' }}>
+                        Please Login
+                    </Typography>
+                </center>
+            </div>
+        );
+    }
 };
 
 export default NussInspector;
