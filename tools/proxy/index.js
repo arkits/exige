53c85f07fccbda9291d849a10d043d136771ecb2
Archit Khode <arkits@outlook.com>
2020-03-09 20:58:23 -0700

    added proxy

---
+++ tools/proxy/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+const PROXY_EP = 'https://host_i_want_to_hit.com';
+
+app.get('/*', async (req, res) => {
+
+    console.log(req.method + ' - ' + PROXY_EP + req.url);
+
+    try {
+
+        let proxy_result = await axios({
+            method: req.method,
+            url: PROXY_EP + req.url,
+            headers: {
+                Authorization: req.headers.authorization
+            }
+        });
+
+        res.status(proxy_result.status);
+        res.json(proxy_result.data);
+
+    } catch (error) {
+
+        res.status(error.response.status);
+        res.json(error.response.data);
+
+    }
+
+    return res;
+
+});
+
+app.listen(port, () => console.log(`Proxy listening on port ${port}!`));
